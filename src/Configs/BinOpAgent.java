5a4e6845d6c3d0610475c0c79314f37ed257540c
plasdje <omer30009@gmail.com>
2024-12-08 22:37:33 +0200

    Update BinOpAgent.java

--- src/Configs/BinOpAgent.java
+++ src/Configs/BinOpAgent.java
@@ -3,8 +3,23 @@ package Configs;
 import java.util.function.BinaryOperator;
 
 import Graph.TopicManagerSingleton.TopicManager;
-
+import Graph.TopicManagerSingleton;
+import Graph.Topic;
 public class BinOpAgent{
+    public final String agentName;
+    public final TopicManager TopicManager;
+    public final Topic input1;
+    public final Topic input2;
+    public final Topic output;
+    public final BinaryOperator<Double> operation;
+    public BinOpAgent(String name, String firstT, String secondT, String output,BinaryOperator<Double> op){
+        this.agentName = name;
+        this.TopicManager = TopicManagerSingleton.get();
+        this.input1 = TopicManager.getTopic(firstT);
+        this.input2 = TopicManager.getTopic(secondT);
+        this.output = TopicManager.getTopic(output);
+        this.operation = op;
 
-    
+    }
+
 }
